63aae895b8a9f66d7ced9f3223460077e3bafa16
quevenne <j.quevenne@ecole-ipssi.net>
2019-09-01 22:04:38 +0200

    finish tp

---
+++ Budget_pro_project_jq/src/Form/CardType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Card;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CardType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('creditCardType')
+            ->add('creditCardNumber')
+            ->add('currencyCode')
+            ->add('value')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Card::class,
+        ]);
+    }
+}
